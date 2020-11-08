5997fc469d9fbb2d7066bcf82b8d46736e1ec198
Jordan <jordan.ttc.design@gmail.com>
2020-11-08 23:45:02 +0800

    add/增加滾動js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -19,4 +19,23 @@ $(document).ready(() => {
   });
   //aos套件
   AOS.init();
+
+  //menu 移動
+  //主要的
+  $(".header__nav li a,.this").click(function (event) {
+    var scrollPoint = 0
+    // console.log('ok');
+    var sectionTitle = this.title;
+    var id = "#home__" + sectionTitle;
+    scrollPoint = $(id).offset().top;
+    var lastScrollPoint = scrollPoint - 180;
+    // console.log(scrollPoint);
+    // console.log(lastScrollPoint);
+    $("html,body").animate({ scrollTop: lastScrollPoint }, 800);
+  });
+  $(".header__nav--sm li a,.this").click(function (event) {
+    $(".navbar-collapse").removeClass('show');
+    //collapse is not a function
+    // $('.collapse').collapse('hide');
+  });
 });
